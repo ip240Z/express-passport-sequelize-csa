4147e106ef5613389e405e3c6e6df69e75caf50e
Ian <ip240z@protonmail.com>
2023-05-01 11:32:35 -0400

    Adds host to config

--- config/config.js
+++ config/config.js
@@ -15,10 +15,9 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "process.env.DATABASE_USER",
-    "password": "process.env.DATABASE_PASSWORD",
-    "database": "process.env.DATABASE_NAME",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
   }
 }
