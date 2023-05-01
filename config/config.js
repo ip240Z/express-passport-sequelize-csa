b480c6027d316d686b281cf95f9d474d0a72b6f1
Ian <ip240z@protonmail.com>
2023-05-01 11:36:50 -0400

    Added dialect postgres to config

--- config/config.js
+++ config/config.js
@@ -19,5 +19,6 @@ module.exports = {
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
     "host": process.env.DATABASE_HOST,
+    "dialect": "postgres"
   }
 }
